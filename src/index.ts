48b4b54848f33c0283cbfce235c636936c834d0b
gmagnani <magnanigeovanni@gmail.com>
2026-04-07 10:54:57 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import "dotenv/config";
 
+// import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+// import fastifyApiReference from "@scalar/fastify-api-reference";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
+
+// import { auth } from "./lib/auth.js";
+// import { aiRoutes } from "./routes/ai.js";
+// import { homeRoutes } from "./routes/home.js";
+// import { meRoutes } from "./routes/me.js";
+// import { statsRoutes } from "./routes/stats.js";
+// import { workoutPlanRoutes } from "./routes/workout-plan.js";
 
 const app = Fastify({
   logger: true,
@@ -15,10 +26,71 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Fit AI API",
+      description: "API para o Fit AI",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+// await app.register(fastifyCors, {
+//   origin: ["http://localhost:3000"],
+//   credentials: true,
+// });
+
+// await app.register(fastifyApiReference, {
+//   routePrefix: "/docs",
+//   configuration: {
+//     sources: [
+//       {
+//         title: "Bootcamp Treinos API",
+//         slug: "bootcamp-treinos-api",
+//         url: "/swagger.json",
+//       },
+//       {
+//         title: "Auth API",
+//         slug: "auth-api",
+//         url: "/api/auth/open-api/generate-schema",
+//       },
+//     ],
+//   },
+// });
+
+// RESTful
+// Routes
+// await app.register(homeRoutes, { prefix: "/home" });
+// await app.register(meRoutes, { prefix: "/me" });
+// await app.register(statsRoutes, { prefix: "/stats" });
+// await app.register(workoutPlanRoutes, { prefix: "/workout-plans" });
+// await app.register(aiRoutes, { prefix: "/ai" });
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  schema: {
+    hide: true,
+  },
+  handler: async () => {
+    return app.swagger();
+  },
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
   schema: {
+    description: "Hello world",
+    tags: ["Hello World"],
     response: {
       200: z.object({
         message: z.string(),
@@ -26,13 +98,50 @@ app.withTypeProvider<ZodTypeProvider>().route({
     },
   },
   handler: () => {
-    return { message: "Hello World" };
+    return {
+      message: "Hello World",
+    };
   },
 });
 
-app.listen({ port: Number(process.env.PORT) || 8080 }, function (err) {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-});
+// app.route({
+//   method: ["GET", "POST"],
+//   url: "/api/auth/*",
+//   async handler(request, reply) {
+//     try {
+//       // Construct request URL
+//       const url = new URL(request.url, `http://${request.headers.host}`);
+
+//       // Convert Fastify headers to standard Headers object
+//       const headers = new Headers();
+//       Object.entries(request.headers).forEach(([key, value]) => {
+//         if (value) headers.append(key, value.toString());
+//       });
+//       // Create Fetch API-compatible request
+//       const req = new Request(url.toString(), {
+//         method: request.method,
+//         headers,
+//         ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+//       });
+//       // Process authentication request
+//       const response = await auth.handler(req);
+//       // Forward response to client
+//       reply.status(response.status);
+//       response.headers.forEach((value, key) => reply.header(key, value));
+//       reply.send(response.body ? await response.text() : null);
+//     } catch (error) {
+//       app.log.error(error);
+//       reply.status(500).send({
+//         error: "Internal authentication error",
+//         code: "AUTH_FAILURE",
+//       });
+//     }
+//   },
+// });
+
+try {
+  await app.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
