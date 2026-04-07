bd4414f8cd0611b8089b7cda9d238b0e995a80a4
gmagnani <magnanigeovanni@gmail.com>
2026-04-07 09:47:33 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-console.log("Hello World");
-    
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen({ port: Number(process.env.PORT) || 8080 }, function (err) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
