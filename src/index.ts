45e1449705bd6a91f6a57e38ac4baef7ba1c7bf3
gmagnani <magnanigeovanni@gmail.com>
2026-04-07 11:59:40 -0300

    chore: add betterauth setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 
-// import fastifyCors from "@fastify/cors";
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
-// import fastifyApiReference from "@scalar/fastify-api-reference";
+import fastifyApiReference from "@scalar/fastify-api-reference";
 import Fastify from "fastify";
 import {
   jsonSchemaTransform,
@@ -12,7 +12,7 @@ import {
 } from "fastify-type-provider-zod";
 import z from "zod";
 
-// import { auth } from "./lib/auth.js";
+import { auth } from "./lib/auth.js";
 // import { aiRoutes } from "./routes/ai.js";
 // import { homeRoutes } from "./routes/home.js";
 // import { meRoutes } from "./routes/me.js";
@@ -43,28 +43,28 @@ await app.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 });
 
-// await app.register(fastifyCors, {
-//   origin: ["http://localhost:3000"],
-//   credentials: true,
-// });
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+});
 
-// await app.register(fastifyApiReference, {
-//   routePrefix: "/docs",
-//   configuration: {
-//     sources: [
-//       {
-//         title: "Bootcamp Treinos API",
-//         slug: "bootcamp-treinos-api",
-//         url: "/swagger.json",
-//       },
-//       {
-//         title: "Auth API",
-//         slug: "auth-api",
-//         url: "/api/auth/open-api/generate-schema",
-//       },
-//     ],
-//   },
-// });
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Bootcamp Treinos API",
+        slug: "bootcamp-treinos-api",
+        url: "/swagger.json",
+      },
+      {
+        title: "Auth API",
+        slug: "auth-api",
+        url: "/api/auth/open-api/generate-schema",
+      },
+    ],
+  },
+});
 
 // RESTful
 // Routes
@@ -104,40 +104,40 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
 });
 
-// app.route({
-//   method: ["GET", "POST"],
-//   url: "/api/auth/*",
-//   async handler(request, reply) {
-//     try {
-//       // Construct request URL
-//       const url = new URL(request.url, `http://${request.headers.host}`);
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      // Construct request URL
+      const url = new URL(request.url, `http://${request.headers.host}`);
 
-//       // Convert Fastify headers to standard Headers object
-//       const headers = new Headers();
-//       Object.entries(request.headers).forEach(([key, value]) => {
-//         if (value) headers.append(key, value.toString());
-//       });
-//       // Create Fetch API-compatible request
-//       const req = new Request(url.toString(), {
-//         method: request.method,
-//         headers,
-//         ...(request.body ? { body: JSON.stringify(request.body) } : {}),
-//       });
-//       // Process authentication request
-//       const response = await auth.handler(req);
-//       // Forward response to client
-//       reply.status(response.status);
-//       response.headers.forEach((value, key) => reply.header(key, value));
-//       reply.send(response.body ? await response.text() : null);
-//     } catch (error) {
-//       app.log.error(error);
-//       reply.status(500).send({
-//         error: "Internal authentication error",
-//         code: "AUTH_FAILURE",
-//       });
-//     }
-//   },
-// });
+      // Convert Fastify headers to standard Headers object
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+      // Create Fetch API-compatible request
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      // Process authentication request
+      const response = await auth.handler(req);
+      // Forward response to client
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
+  },
+});
 
 try {
   await app.listen({ port: Number(process.env.PORT) || 8081 });
